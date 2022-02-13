08fe56b65e2be8f6252314efabf3976c73722990
shuvo-99 <anontoshuvo99@gmail.com>
2022-02-13 13:46:13 +0600

    pin-generate

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-function getpin() {
+function getPin() {
   const pin = Math.round(Math.random() * 10000);
   const pinString = pin + "";
   if (pinString.length == 4) {
     return pin;
   } else {
-    return getpin();
+    return getPin();
   }
 }
 
-function generatepin() {
-  const pin = getpin();
+function generatePin() {
+  const pin = getPin();
   document.getElementById("display-pin").value = pin;
+  console.log(pin);
 }
