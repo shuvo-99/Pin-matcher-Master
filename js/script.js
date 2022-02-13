208cb79cfe3e414bc4a477b6fd089cb97efef22e
shuvo-99 <anontoshuvo99@gmail.com>
2022-02-13 14:11:39 +0600

    calc input

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@ function generatePin() {
   document.getElementById("display-pin").value = pin;
   console.log(pin);
 }
+
+document.getElementById("key-pad").addEventListener("click", function (event) {
+  const number = event.target.innerText;
+  const calcInput = document.getElementById("typed-numbers");
+  if (isNaN(number)) {
+    if (number == "C") {
+      calcInput.value = "";
+    }
+  } else {
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+  }
+});
